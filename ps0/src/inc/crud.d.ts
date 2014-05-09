0f1c1a047cab040c32d3f9b9bc8bfbd5e86127d1
Tanuja Joshi <joshitanuja@hotmail.com>
2014-05-08 19:50:46 -0700

    Moved CRUD interface to crud.d.ts
    Renamed apis on PSDB interface as per latest recommendation of
    add/find/update/deleteObj.
    Changed PSDBError to Error, PSDBCallBack ->SimpleCallBack, Added
    CallBackWithCount
    Renamed PSDBCRUD to DBCRUD and Moved to separate crud.d.ts file

---
+++ ps0/src/inc/crud.d.ts
@@ -0,0 +1,55 @@
+﻿//
+//   MODULE: DBCRUD
+//   Copyright (c) 2014 Eastside Web (http://eastsideweb.github.io/)
+//
+//   FILE: crud.d.ts
+//   DESCRIPTION: File containing the interface supported by the module that provides low level CRUD database capabilities
+//
+//   HISTORY:
+//     Date            By  Comment
+//     2014 May 5th    TJ  Created
+// 
+
+// Typical use of this object will be 
+// var dbCRUD = require('DBCRUD').createDBHandle('server', 'dbName');
+interface DBCRUDModule {
+    createDBHandle(server: string, dbName: string): DBCRUD;
+}
+
+interface DBCRUD {
+
+    // /*Testing purpose*/
+    // Allowed collection strings:
+    //  all SeriesObjectTypes / eventPuzzleStates* 
+
+    // Insert a new document in the given collection
+    // Allowed objMap cannot have _id. Could have name, description, active, status, teamLead, players[], puzzles[]
+    insertObj(collection: string, objMap: any, callback: (err: Error, obj: any) => void): void;
+
+    // Find objects from the given collection
+    // Allowed collection strings:
+    //  PuzzleSeriesInfo/all SeriesObjectTypes / eventPuzzleStates* 
+    // Allowed find_map: {_id: string, name: string}
+    findObj(collection: string, findMap: any, projMap: any, callback: (err: Error, objList: any[]) => void): void;
+
+
+    // Update an existing document in the given collection 
+    // Allowed find_map: {_id: stringps
+    // Allowed collection strings:
+    //  all SeriesObjectTypes / eventPuzzleStates* 
+    // Allowed set_map: {name: string, description: string, active: true/false, teamLead: string}
+    // Additional allowed set_maps only for team collection: 
+    //          {$addToSet: { players: { $each: [id1, id2, id3] } }
+    //          {$pull: {playerid1, playerid2,..} //no multi-pull available
+    // Additional allowed set_map only for event collection: 
+    //          {$addToSet: { puzzles: { $each: [id1, id2, id3] } }
+    //          {$pull: {puzzleid1, puzzleid2,..} //no multi-pull available
+    updateObj(collection: string, findMap: any, setMap: any, callback: CallBackWithCount): void;
+
+    // Delete an object from given collection
+    // Allowed collection strings:
+    // all SeriesObjectTypes / eventPuzzleStates* 
+    // Allowed findMap: {_id: string}
+    deleteObj(collection: string, findMap: any, callback: CallBackWithCount): void;
+}
+ 
--- ps0/src/inc/psdb.d.ts
+++ ps0/src/inc/psdb.d.ts
@@ -1,3 +1,16 @@
+//
+//   MODULE: PSDB 
+//   Copyright (c) 2014 Eastside Web (http://eastsideweb.github.io/)
+//
+//   FILE: psdb.d.ts
+//   DESCRIPTION: File containing the main psdb interface used by the server
+//
+//   HISTORY:
+//     Date            By  Comment
+//     2014 May 5th    TJ  Created
+//
+//
+
 // Describes interfaces implemented by PSDB library objects
 interface SeriesInfo {
     name: string;
@@ -5,13 +18,14 @@ interface SeriesInfo {
     description: string;
 }
 
-interface PSDBCallBack {
-    (err: PSDBError): void;
+interface SimpleCallBack {
+    (err: Error): void;
 }
 
-interface PSDBError extends Error {
-
+interface CallBackWithCount {
+    (err: Error, count: number): void;
 }
+
 declare enum SeriesObjectType {
     'instructor',
     'puzzle',
@@ -51,22 +65,23 @@ interface PSDB {
     // queryFields - the select clauses to get a subset of SeriesInfo objects
     // REVIEW: Removing this projection argument cause we want keep the SeriesInfo type specification. 
     // fieldsReturned - the projection that identifies 
-    findSeries(queryFields: any, /* fieldsReturned: any, */callback: (err: PSDBError, list: Array<SeriesInfo>) => void): void;
+    findSeries(queryFields: any, /* fieldsReturned: any, */callback: (err: Error, list: Array<SeriesInfo>) => void): void;
 
     // Function to get a token that represents access to the series object for a given series id, with previliges appropriate 
     // for the given "role". The credentials provided should have the given role previliges
     // Current Supported roles: 'administrator', 'instructor', 'player'
+    // Options provide any options that are to be set for this particular session e.g. {debug: true}
     // Possible errors: 
     //      "InvalidSeriesId"           "series id not found"
     //      "InvalidRoleType"           "Invalid role"
     //      "InvalidCredentials"        "Invalid credentials"
     //      "RoleNotSupportedForUser"   "User not authorized for role" 
-    getSeriesToken(id: string, role: RoleType, credentials: Credentials, callback: (err: PSDBError, token: string) => void): void;
+    getSeriesToken(id: string, role: RoleType, credentials: Credentials, options: any, callback: (err: Error, token: string) => void): void;
 
     // Function to release a token that was previously obtained using getSeriesToken api
     // Possible errors: 
     //      "InvalidTokenId"    "Invalid token"
-    releaseSeriesToken(token: string, callback: PSDBCallBack): void;
+    releaseSeriesToken(token: string, callback: SimpleCallBack): void;
 
     // Synchronous call to get a puzzleSeries object represented by given token (which was handed out earlier)
     // return value will be null if the token is invalid or has expired
@@ -83,29 +98,29 @@ interface PuzzleSeries {
     //      "InvalidObjType"        "Invalid object type"
     //      "InvalidObjId"          "Invaid object id"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    activate(objType: SeriesObjectType, objId: string, callback: PSDBCallBack): void;
-    deactivate(objType: SeriesObjectType, objId: string, callback: PSDBCallBack): void;
+    activate(objType: SeriesObjectType, objId: string, callback: SimpleCallBack): void;
+    deactivate(objType: SeriesObjectType, objId: string, callback: SimpleCallBack): void;
 
     // Update static fields of a given object from the given SeriesObjectType collection having given objId
     // Possible err:
     //      "InvalidObjType"        "Invalid object type"
     //      "InvalidObjId"          "Invaid object id"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    update(objType: SeriesObjectType, objId: string, updateFields: any, callback: (err: PSDBError, updateCount: number) => void): void;
+    updateObj(objType: SeriesObjectType, objId: string, updateFields: any, callback: CallBackWithCount): void;
 
     // Add a new object of given type
     // Possible errors:
     //      "InvalidObjType"        "Invalid object type"
     //      "EmptyName"             "name field missing"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    add(objType: SeriesObjectType, objInfo: any, callback: (err: PSDBError, objInfo: SeriesObject) => void): void;
+    addObj(objType: SeriesObjectType, objInfo: any, callback: (err: Error, objInfo: SeriesObject) => void): void;
 
     // Delete an object with given object id from the series
     // Possible errors:
     //      "InvalidObjType"        "Invalid object type"
     //      "InvalidObjId"          "Invaid object id"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    delete(objType: SeriesObjectType, objInfo: any, callback: (PSDBError, updateCount: number) => void): void;
+    deleteObj(objType: SeriesObjectType, objInfo: any, callback: CallBackWithCount): void;
 
     // Add given set of players to the given team
     // Possible errors:
@@ -114,7 +129,7 @@ interface PuzzleSeries {
     //      "PlayerNotActive"       "One or more player is deactivate"
     //      "PlayerOnAnotherTeam"   "One or more player is already part of another team"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    addPlayersToTeam(listPlayerIds: string[], teamId: string, callback: PSDBCallBack): void;
+    addPlayersToTeam(listPlayerIds: string[], teamId: string, callback: SimpleCallBack): void;
 
     // Remove given set of players from the given team
     // Possible errors:
@@ -122,21 +137,21 @@ interface PuzzleSeries {
     //      "InvalidTeamId"         "Invalid team id"
     //      "PlayerNotOnTeam"       "One or more player is not part of the team"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    removePlayersFromTeam(listPlayerIds: string[], teamId: string, callback: (PSDBError, updateCount: number) => void): void;
+    removePlayersFromTeam(listPlayerIds: string[], teamId: string, callback: CallBackWithCount): void;
 
     // Get a list of fields of objects of given SeriesObjectType meeting the select-query condition
     // Possible errors:
     //      "InvalidObjType"        "Invalid object type"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    find(objType: SeriesObjectType, queryFields: any, fieldsReturned: any, callback: (err: PSDBError, list: any[]) => void): void;
+    findObj(objType: SeriesObjectType, queryFields: any, fieldsReturned: any, callback: (err: Error, list: any[]) => void): void;
 
     // Set status of an event. 
     // Possible errors:
-    //      "Invalid event id"
-    //      "event already started"
-    //      "another event already active"
+    //      "InvalidEventId"        "Invalid event id"
+    //      "InvalidStatusChange"   "attemp to start an event already underway or ended OR attempt to end an event already ended"
+    //      "InvalidEventChange"    "another event already active"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    setEventStatus(eventId: string, eventStatus: EventStatus, callback: PSDBCallBack): void;
+    setEventStatus(eventId: string, eventStatus: EventStatus, callback: SimpleCallBack): void;
 
     // Assign given set of puzzles to the given team
     // Possible errors:
@@ -144,7 +159,7 @@ interface PuzzleSeries {
     //      "InvalidTeamId"         "Invalid team id"
     //      "PuzzleNotInEvent"      "One or more puzzles not part of the current event"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    assignPuzzlesToTeam(listPuzzleIds: string[], teamId: string, callback: PSDBCallBack): void;
+    assignPuzzlesToTeam(listPuzzleIds: string[], teamId: string, callback: SimpleCallBack): void;
 
     // Remove given set of puzzles from the given team
     // Possible errors:
@@ -152,7 +167,7 @@ interface PuzzleSeries {
     //      "InvalidTeamId"         "Invalid team id"
     //      "PuzzleNotInTeam"       "One or more puzzles not assigned to the team"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    removePuzzlesFromTeam(listPuzzleIds: string[], teamId: string, callback: PSDBCallBack): void;
+    removePuzzlesFromTeam(listPuzzleIds: string[], teamId: string, callback: SimpleCallBack): void;
 
     // Update the state of the given puzzle for the given team
     // Possible errors:
@@ -160,50 +175,6 @@ interface PuzzleSeries {
     //      "InvalidTeamId"         "Invalid team id"
     //      "PuzzleNotInEvent"      "puzzle not assigned to the team"
     //      "UnauthorizedAccess"    "Access to this api not supported for the RoleType"
-    updatePuzzleState(teamID: string, puzzleID: string, puzzleState: any, callback: PSDBCallBack): void;
+    updatePuzzleState(teamID: string, puzzleID: string, puzzleState: any, callback: SimpleCallBack): void;
 
-}
-
-// Interface supported by the CRUD module that the PuzzleSeries object uses to perform all the database operations
-// Typical use of this object will be 
-// var psdbCRUD = require('PSDBCRUD').createDBHandle('mongoserver', 'mongodb');
-interface PSDBCRUDModule {
-    createDBHandle(server: string, dbName: string): PSDBCRUD;
-}
-
-interface PSDBCRUD {
-
-    // /*Testing purpose*/
-    // Allowed collection strings:
-    //  all SeriesObjectTypes / eventPuzzleStates* 
-
-    // Insert a new document in the given collection
-    // Allowed objMap cannot have _id. Could have name, description, active, status, teamLead, players[], puzzles[]
-    insertObj(collection: string, objMap: any, callback: (err: PSDBError, obj: any) => void): void;
-
-    // Find objects from the given collection
-    // Allowed collection strings:
-    //  PuzzleSeriesInfo/all SeriesObjectTypes / eventPuzzleStates* 
-    // Allowed find_map: {_id: string, name: string}
-    findObj(collection: string, findMap: any, projMap: any, callback: (err: PSDBError, objList: any[]) => void): void;
-
-
-    // Update an existing document in the given collection 
-    // Allowed find_map: {_id: string}
-    // Allowed collection strings:
-    //  all SeriesObjectTypes / eventPuzzleStates* 
-    // Allowed set_map: {name: string, description: string, active: true/false, teamLead: string}
-    // Additional allowed set_maps only for team collection: 
-    //          {$addToSet: { players: { $each: [id1, id2, id3] } }
-    //          {$pull: {playerid1, playerid2,..} //no multi-pull available
-    // Additional allowed set_map only for event collection: 
-    //          {$addToSet: { puzzles: { $each: [id1, id2, id3] } }
-    //          {$pull: {puzzleid1, puzzleid2,..} //no multi-pull available
-    updateObj(collection: string, findMap: any, setMap: any, callback: (PSDBError, updateCount: number) => void): void;
-
-    // Delete an object from given collection
-    // Allowed collection strings:
-    // all SeriesObjectTypes / eventPuzzleStates* 
-    // Allowed findMap: {_id: string}
-    deleteObj(collection: string, findMap: any, callback: (err: PSDBError, deleteCount: number) => void): void;
 }
