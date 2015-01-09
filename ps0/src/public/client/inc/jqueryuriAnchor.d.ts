c2fb0ee4e22c06d1d545778df21ffcbf2547206c
joshitanuja <joshitanuja@hotmail.com>
2015-01-09 12:27:07 -0800

    Added jqueryuriAnchor plugin and d.ts

---
+++ ps0/src/public/client/inc/jqueryuriAnchor.d.ts
@@ -0,0 +1,13 @@
+﻿// Type definitions for jquery.uriAnchor v 1.1.3
+// Project: https://github.com/mmikowski/urianchor
+
+interface uriAnchor {
+    configModule(arg_map): void;
+    getVarType(data): string;
+    makeAnchorMap(): any;
+    makeAnchorString(anchor_map_in, option_map_in?  ): any;
+    setAnchor(anchor_map, option_map, replace_flag): void;
+}
+interface JQueryStatic {
+    uriAnchor: uriAnchor;
+}
