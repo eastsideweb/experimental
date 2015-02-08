68a076ffccf7dbb7dd42ef65a8dac99b0240bb7c
joshitanuja <joshitanuja@hotmail.com>
2015-02-07 18:03:16 -0800

    batch file to setup mongohq/compose db via import
    batch file to setup mongohq/compose db via import

---
+++ ps0/src/test/initTestDB/testSeriesId1/initDBtestSeriesId1-Production.bat
@@ -0,0 +1,8 @@
+%mongoimport%\mongoimport.exe -v --host oceanic.mongohq.com:10065 --db TPTest2 -u tanujaj -p a1B2c3 --drop --jsonArray  --file .\events.json
+%mongoimport%\mongoimport.exe -v --host oceanic.mongohq.com:10065 --db TPTest2 -u tanujaj -p a1B2c3 --drop --jsonArray  --file .\puzzles.json
+%mongoimport%\mongoimport.exe -v --host oceanic.mongohq.com:10065 --db TPTest2 -u tanujaj -p a1B2c3 --drop --jsonArray  --file .\teams.json
+%mongoimport%\mongoimport.exe -v --host oceanic.mongohq.com:10065 --db TPTest2 -u tanujaj -p a1B2c3 --drop --jsonArray  --file .\instructors.json
+%mongoimport%\mongoimport.exe -v --host oceanic.mongohq.com:10065 --db TPTest2 -u tanujaj -p a1B2c3 --drop --jsonArray  --file .\players.json
+%mongoimport%\mongoimport.exe -v --host oceanic.mongohq.com:10065 --db TPTest2 -u tanujaj -p a1B2c3 --drop --jsonArray  --file .\puzzleStates_events1.json
+%mongoimport%\mongoimport.exe -v --host oceanic.mongohq.com:10065 --db TPTest2 -u tanujaj -p a1B2c3 --drop --jsonArray  --file .\annotations.json
+%mongoimport%\mongoimport.exe -v --host oceanic.mongohq.com:10065 --db TPTest2 -u tanujaj -p a1B2c3 --drop --jsonArray  --file .\psdbCounters.json
