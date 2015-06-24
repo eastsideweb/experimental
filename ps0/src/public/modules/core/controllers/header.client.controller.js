7196c608a69af0ff99e83782c824ea384d879a6f
joshitanuja <joshitanuja@hotmail.com>
2015-06-24 11:21:24 -0700

    Added signout functionality
    Added signout functionality by adding a method on the header.controller
    that sends a delete request

--- ps0/src/public/modules/core/controllers/header.client.controller.js
+++ ps0/src/public/modules/core/controllers/header.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$scope', '$http', '$location', 'Authentication', 'Menus',
+	function($scope, $http, $location, Authentication, Menus) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
@@ -10,6 +10,15 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 			$scope.isCollapsed = !$scope.isCollapsed;
 		};
 
+		$scope.signout = function () {
+		    var deleteUrl = '/series/' + $scope.authentication.user.seriesId + '/session/' + $scope.authentication.user.token;
+		    $http.delete(deleteUrl).success(function () {
+		        delete $scope.authentication.user;
+		        $location.path('/');
+		    }).error(function () {
+		        $location.path('/');
+		    })
+		}
 		// Collapsing the menu after navigation
 		$scope.$on('$stateChangeSuccess', function() {
 			$scope.isCollapsed = false;
