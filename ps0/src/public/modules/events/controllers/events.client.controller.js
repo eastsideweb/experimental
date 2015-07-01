dc1801ee3177d284e7301da2a6968b76d9df6b61
maryannjoy <maryannjoy@hotmail.com>
2015-07-01 16:31:24 -0700

    Added sublists to event object
    added puzzles, teams, instructors & players to events view

--- ps0/src/public/modules/events/controllers/events.client.controller.js
+++ ps0/src/public/modules/events/controllers/events.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Events controller
-angular.module('events').controller('EventsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Events',
-	function($scope, $stateParams, $location, Authentication, Events) {
+angular.module('events').controller('EventsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Events','Players', 'Puzzles','Instructors','Teams',
+	function($scope, $stateParams, $location, Authentication, Events, Players, Puzzles, Instructors, Teams) {
 		$scope.authentication = Authentication;
 
 		// Create new Event
@@ -11,7 +11,12 @@ angular.module('events').controller('EventsController', ['$scope', '$stateParams
 			var event = new Events ({
 			    name: this.name,
 			    description: this.description,
-                active: this.active
+                active: this.active,
+                status: this.status,
+                playerIds: this.playerIds,
+                puzzleIds: this.puzzleIds,
+                instructorIds: this.instructorIds,
+                teamIds: this.teamIds
 			});
 
 			// Redirect after save
@@ -19,7 +24,13 @@ angular.module('events').controller('EventsController', ['$scope', '$stateParams
 				$location.path('events/' + response._id);
 
 				// Clear form fields
-				$scope.name = '';
+                $scope.name = '';
+                $scope.description = '';
+                $scope.playerIds = [];
+                $scope.puzzleIds = [];
+                $scope.teamIds = [];
+                $scope.instructorIds = [];
+                       
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
@@ -60,10 +71,78 @@ angular.module('events').controller('EventsController', ['$scope', '$stateParams
 		};
 
 		// Find existing Event
-		$scope.findOne = function() {
-			$scope.event = Events.get({ 
-				eventId: $stateParams.eventId
-			});
-		};
+        $scope.findOne = function () {
+            $scope.event = Events.get({
+                eventId: $stateParams.eventId
+            },
+            function () {
+                // get the names of the players
+                if ($scope.event.playerIds.length !== 0) {
+                   var playerIds = concatIds($scope.event.playerIds);
+                    $scope.players = Players.query({
+                        "properties": "name",
+                        "_id": playerIds
+                    });
+                }
+                else {
+                    $scope.players = [];
+                }
+                // get the names of the puzzles
+                if ($scope.event.puzzleIds.length !== 0) {
+                    var puzzleIds = concatIds($scope.event.puzzleIds);
+                    $scope.puzzles = Puzzles.query({
+                        "properties": "name",
+                        "_id": puzzleIds
+                    });
+                }
+                else {
+                    $scope.puzzles = [];
+                }
+                // get the names of the teams
+                if ($scope.event.teamIds.length !== 0) {
+                    var teamIds = concatIds($scope.event.teamIds);
+                    $scope.teams = Teams.query({
+                        "properties": "name",
+                        "_id": teamIds
+                    });
+                }
+                else {
+                    $scope.teams = [];
+                }
+                // get the names of the instructors
+                if ($scope.event.instructorIds.length !== 0) {
+                    var instructorIds = concatIds($scope.event.instructorIds);
+                    $scope.instructors = Instructors.query({
+                        "properties": "name",
+                        "_id": instructorIds
+                    });
+                }
+                else {
+                    $scope.instructors = [];
+                }
+            });
+        };
+        $scope.togglePlayerMenu = function () {
+            $scope.showPlayerMenu = !$scope.showPlayerMenu;
+        };
+        $scope.togglePuzzleMenu = function () {
+            $scope.showPuzzleMenu = !$scope.showPuzzleMenu;
+        };
+        $scope.toggleTeamMenu = function () {
+            $scope.showTeamMenu = !$scope.showTeamMenu;
+        };
+        $scope.toggleInstructorMenu = function () {
+            $scope.showInstructorMenu = !$scope.showInstructorMenu;
+        };
+        function concatIds(objectIds){
+            var tempIds = "";
+            objectIds.forEach(function (item, index) {
+                tempIds = tempIds.concat(item);
+                if (index !== objectIds.length - 1) {
+                    tempIds = tempIds.concat(',');
+                }
+            });
+            return tempIds;
+        }
 	}
 ]);
