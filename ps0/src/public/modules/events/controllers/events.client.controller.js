0c5b466867b7c01f27430ed8eac4e6dcb17c4678
maryannjoy <maryannjoy@hotmail.com>
2015-07-01 07:16:49 -0700

    Added Event object in the client
    copied and edited files from instructor directory, followed steps
    outlined in 'Added the player...' checkin notes. Have not added sublists

---
+++ ps0/src/public/modules/events/controllers/events.client.controller.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// Events controller
+angular.module('events').controller('EventsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Events',
+	function($scope, $stateParams, $location, Authentication, Events) {
+		$scope.authentication = Authentication;
+
+		// Create new Event
+		$scope.create = function() {
+			// Create new Event object
+			var event = new Events ({
+			    name: this.name,
+			    description: this.description,
+                active: this.active
+			});
+
+			// Redirect after save
+			event.$save(function(response) {
+				$location.path('events/' + response._id);
+
+				// Clear form fields
+				$scope.name = '';
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Remove existing Event
+		$scope.remove = function(event) {
+			if ( event ) { 
+				event.$remove();
+
+				for (var i in $scope.events) {
+					if ($scope.events [i] === event) {
+						$scope.events.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.event.$remove(function() {
+					$location.path('events');
+				});
+			}
+		};
+
+		// Update existing Event
+		$scope.update = function() {
+			var event = $scope.event;
+			var id = event._id;
+			event.$update(function () {
+			    // $location.path('events/' + event._id); <-- Doesn't work
+				$location.path('events/' + id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.title;
+			});
+		};
+
+		// Find a list of Events
+		$scope.find = function() {
+			$scope.events = Events.query();
+		};
+
+		// Find existing Event
+		$scope.findOne = function() {
+			$scope.event = Events.get({ 
+				eventId: $stateParams.eventId
+			});
+		};
+	}
+]);
