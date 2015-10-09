1913a812042e5cb7b3de6577b5f6dc9acf550500
Joseph Joy <josephmjoy@hotmail.com>
2015-10-08 17:43:52 -0700

    Sample Puzzle Oracle Log Processing console app
    Starting code for the sample app that watches a directory for Puzzle
    Oracle submission log files and processes each file as it shows up.

---
+++ pzo/PuzzleOracleV0/LogProcessorSample/LogProcessor.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogProcessorSample
+{
+    class LogEventArgs : EventArgs
+    {
+        public String transactionId;
+        public String timestamp;
+        public String teamId;
+        public String puzzleId;
+        public String status;
+        public String extraText;
+    }
+    class LogProcessor : IDisposable
+    {
+        private string logDirectory;
+        FileSystemWatcher watcher;
+        List<EventHandler<LogEventArgs>> ehList;
+        bool active = false;
+
+        public LogProcessor(string logDirectory)
+        {
+            ehList = new List<EventHandler<LogEventArgs>>();
+            // TODO: Complete member initialization
+            this.logDirectory = logDirectory;
+            watcher = new FileSystemWatcher();
+            watcher.Path = logDirectory;
+            /* Watch for changes in LastAccess and LastWrite times, and
+               the renaming of files or directories. */
+            watcher.NotifyFilter = NotifyFilters.FileName; //| NotifyFilters.DirectoryName;
+            // Only watch text files.
+            watcher.Filter = "*.csv";
+
+            // Add event handlers.
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            //watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            //watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+
+
+        }
+        // Define the event handlers.
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            if (!active)
+            {
+                return;
+            }
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+            // TODO - read all the file content and call the handler.
+            foreach (var eh in ehList)
+            {
+                //
+            }
+        }
+
+        internal void registerEventHandler(EventHandler<LogEventArgs> eh)
+        {
+            ehList.Add(eh);
+        }
+
+        internal void start()
+        {
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+            active = true;
+        }
+
+        internal void stop()
+        {
+            active = false;
+            // Stop watching.
+            watcher.EnableRaisingEvents = false;
+            throw new NotImplementedException();
+        }
+
+        void IDisposable.Dispose()
+        {
+            watcher.EnableRaisingEvents = false;
+        }
+    }
+}
---
+++ pzo/PuzzleOracleV0/LogProcessorSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogProcessorSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String logDirectory = "C:\\Users\\josephj\\Documents\\SCRATCH\\new";
+            using (LogProcessor lp = new LogProcessor(logDirectory)) {
+            lp.registerEventHandler((s,e) => myLogEventHandler(s,e));
+            lp.start();
+            Console.WriteLine("Enter something to quit.");
+            String input = Console.ReadLine();
+            lp.stop();
+            }
+        }
+
+        //delegate<>
+        static void myLogEventHandler(object sender, LogEventArgs e) {
+            Console.WriteLine(e);
+        }
+    }
+
+
+}
