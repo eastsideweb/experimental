d0faade8f137c21c1c21844838d4ace6aa88a5c4
Joseph Joy <josephmjoy@hotmail.com>
2015-10-08 19:19:37 -0700

    Mostly complete
    Implemented: Parse newly created files and report back the parsed
    entries.

--- pzo/PuzzleOracleV0/LogProcessorSample/LogProcessor.cs
+++ pzo/PuzzleOracleV0/LogProcessorSample/LogProcessor.cs
@@ -7,14 +7,36 @@ using System.IO;
 
 namespace LogProcessorSample
 {
+    class LogEntry
+    {
+        public bool valid = false; // if not valid parseError will contain the error.
+        public String transactionId = "";
+        public String timestamp = "";
+        public String teamId = "";
+        public String teamName = "";
+        public String puzzleId = "";
+        public String status = "";
+        public String extraText = "";
+
+        public String parseError = "";
+        public int rowIndex = 0; // row# in log file.
+
+        public LogEntry(int rowIndex)
+        {
+            this.rowIndex = rowIndex;
+        }
+    }
+
     class LogEventArgs : EventArgs
     {
-        public String transactionId;
-        public String timestamp;
-        public String teamId;
-        public String puzzleId;
-        public String status;
-        public String extraText;
+        public String logPath;
+        public LogEntry[] entries;
+
+        public LogEventArgs(String path, LogEntry[] entries)
+        {
+            this.logPath = path;
+            this.entries = entries;
+        }
     }
     class LogProcessor : IDisposable
     {
@@ -53,10 +75,60 @@ namespace LogProcessorSample
             }
             Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
             // TODO - read all the file content and call the handler.
+
+            processLogFile(e.FullPath);
+
+
+        }
+
+        private void processLogFile(string logFile)
+        {
+            String[] lines;
+
+            using (TextReader tr = new StreamReader(logFile))
+            {
+                String allText = tr.ReadToEnd();
+                String[] delims = { "\r\n" };
+                lines = allText.Split(delims, StringSplitOptions.None);
+                tr.Close();
+            }
+            LogEntry[] logEntries = new LogEntry[lines.Length]; // could be of length 0.
+            for (int i = 0; i < lines.Length; i++)
+            {
+                String row = lines[i];
+                LogEntry le = parseLogRow(i, row);
+                logEntries[i] = le;
+            }
+
+            LogEventArgs lea = new LogEventArgs(logFile, logEntries);
             foreach (var eh in ehList)
             {
-                //
+                eh(this, lea);
             }
+
+        }
+
+        private LogEntry parseLogRow(int rowIndex, string row)
+        {
+            String[] fields = row.Split(',');
+            LogEntry le = new LogEntry(rowIndex);
+            if (fields.Length != 7)
+            {
+                le.valid = false;
+                le.parseError = "Incorrect field count.";
+                return le; // ************* EARLY RETURN *****************
+            }
+
+            le.transactionId = fields[0];
+            le.timestamp = fields[1];
+            le.teamId = fields[2];
+            le.teamName = fields[3];
+            le.puzzleId = fields[4];
+            le.status = fields[5];
+            le.extraText = fields[6];
+            le.parseError = "";
+            le.valid = false; // TODO - haven't done any verification yet...
+            return le;
         }
 
         internal void registerEventHandler(EventHandler<LogEventArgs> eh)
@@ -76,7 +148,7 @@ namespace LogProcessorSample
             active = false;
             // Stop watching.
             watcher.EnableRaisingEvents = false;
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         void IDisposable.Dispose()
--- pzo/PuzzleOracleV0/LogProcessorSample/Program.cs
+++ pzo/PuzzleOracleV0/LogProcessorSample/Program.cs
@@ -22,7 +22,26 @@ namespace LogProcessorSample
 
         //delegate<>
         static void myLogEventHandler(object sender, LogEventArgs e) {
-            Console.WriteLine(e);
+            Console.WriteLine("Processing entries from log file " + e.logPath);
+            Console.WriteLine("");
+            foreach (var le in e.entries)
+            {
+                String s = String.Format("{0},{1},{2},{3},{4},{5},{6}",
+                    le.transactionId,
+                    le.timestamp,
+                    le.teamId,
+                    le.teamName,
+                    le.puzzleId,
+                    le.status,
+                    le.extraText
+                    );
+                String suffix = "(OK)";
+                if (!le.valid) {
+                    suffix = String.Format("(INVALID - {0})", le.parseError);
+                }
+                Console.WriteLine(le.rowIndex + ":" + s + suffix);
+            }
+            Console.WriteLine("End of entries from log file " + e.logPath);
         }
     }
 
